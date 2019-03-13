ee907786586c99b07401c3e66fdf693195ce5b7f
rhyderQuinlan <rhyderquinlan@gmail.com>
2019-03-13 01:15:36 +0000

    Commit after lecture 42

--- albums/index.js
+++ albums/index.js
@@ -1,7 +1,7 @@
 // import library to create a Component
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
-import Header from './src/components/header';
+import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
 //create a component
