d2fd322314a46d4370c0bc980a43e55a022befda
rhyderQuinlan <rhyderquinlan@gmail.com>
2019-03-13 10:40:53 +0000

    Commit with error ./CardSection not found

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
-  return (
+const AlbumDetail = (props) => (
     <Card>
-      <Text>{props.album.title}</Text>
+      <CardSection>
+        <Text>{props.album.title}</Text>
+      </CardSection>
     </Card>
   );
-};
 
 export default AlbumDetail;
